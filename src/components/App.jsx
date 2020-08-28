fadb52b3607c252831a22943fa6149cb85534be1
Clifton Long <56210811+Clifton893@users.noreply.github.com>
2020-08-27 23:30:30 -0500

    Upload src files

---
+++ src/components/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Entry from "./Entry";
+import emojipedia from "../emojipedia";
+
+function createEntry(asianEmoji) {
+  return (
+    <Entry
+      key={asianEmoji.id}
+      emoji={asianEmoji.emoji}
+      name={asianEmoji.name}
+      description={asianEmoji.meaning}
+    />
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>asian food emojis</span>
+      </h1>
+
+      <dl className="dictionary">{emojipedia.map(createEntry)}</dl>
+    </div>
+  );
+}
+
+export default App;
